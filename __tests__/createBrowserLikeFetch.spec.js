47d49dfd3d11487bed537906302864a5079000dc
Jonny Adshead <JAdshead@users.noreply.github.com>
2020-08-28 11:41:52 -0700

    fix(createBrowserLikeFetch): remove null cookie options (#9)

--- __tests__/createBrowserLikeFetch.spec.js
+++ __tests__/createBrowserLikeFetch.spec.js
@@ -46,6 +46,29 @@ describe('createCookiePassingFetch', () => {
     expect(setCookie.mock.calls[0][2].maxAge).toEqual(3600000);
   });
 
+  it('does not add missing properties', async () => {
+    const mockFetch = jest.fn(() => Promise.resolve({
+      headers: new Headers({
+        'set-cookie': [
+          'sessionId=1234rlakjhf; Domain=example.com; Path=/path/; HttpOnly;',
+        ],
+      }),
+    }));
+    const hostname = 'api.example.com';
+    const setCookie = jest.fn();
+    const fetchWithRequestHeaders = createBrowserLikeFetch({
+      hostname,
+      setCookie,
+    })(mockFetch);
+
+    await fetchWithRequestHeaders('https://example.com', {
+      credentials: 'include',
+    });
+
+    // if null values are not removed this will fail
+    expect(setCookie.mock.calls[0][2].maxAge).toBe(undefined);
+  });
+
   it('does not call setCookie with mismatching domain on response', async () => {
     const mockFetch = jest.fn(() => Promise.resolve({
       headers: new Headers({
